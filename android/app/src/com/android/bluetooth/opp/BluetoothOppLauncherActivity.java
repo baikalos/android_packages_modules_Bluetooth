6fe73fc19d977e7aef19b43bc837c3c4a51eb964
Sergei Vasilev <svasiliev22@yandex.ru>
2026-01-10 17:18:14 +0300

    Baikalify 1
    Change-Id: I74ef0c59c4aaf3f505ad10a85cf9f1f0a1150785

--- android/app/src/com/android/bluetooth/opp/BluetoothOppLauncherActivity.java
+++ android/app/src/com/android/bluetooth/opp/BluetoothOppLauncherActivity.java
@@ -501,6 +501,11 @@ public class BluetoothOppLauncherActivity extends Activity {
             return true;
         }
 
+        boolean baikal = Settings.Global.getInt(resolver,
+                Settings.Global.BAIKALOS_AIRPLANE_DONT_TOGGLE_BT, 0) == 1;
+
+        if( baikal ) return true;
+
         // Check if airplane mode matters
         final String airplaneModeRadios =
                 Settings.System.getString(resolver, Settings.Global.AIRPLANE_MODE_RADIOS);
--- system/stack/a2dp/a2dp_vendor_aptx_hd_encoder.cc
+++ system/stack/a2dp/a2dp_vendor_aptx_hd_encoder.cc
@@ -33,6 +33,7 @@
 #include "common/time_util.h"
 #include "internal_include/bt_target.h"
 #include "osi/include/allocator.h"
+#include "osi/include/properties.h"
 #include "stack/include/bt_hdr.h"
 
 using namespace bluetooth;
@@ -179,6 +180,9 @@ static void a2dp_vendor_aptx_hd_encoder_update(A2dpCodecConfig* a2dp_codec_confi
   log::info("sample_rate={} bits_per_sample={} channel_count={}", p_feeding_params->sample_rate,
             p_feeding_params->bits_per_sample, p_feeding_params->channel_count);
   a2dp_vendor_aptx_hd_feeding_reset();
+
+  osi_property_set("baikal.last.a2dp_codec","APTX HD");
+  osi_property_set("baikal.last.a2dp_bitrate", "0");
 }
 
 void a2dp_vendor_aptx_hd_encoder_cleanup(void) {
